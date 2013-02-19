b6c5b3f1dd3774c4b70a07be4c674c2f3588ff61
Matt Hernandez <matt@eventargs.com>
2013-02-18 20:18:31 -0500

    reworked the form to use a local script rather than attempting to use the existing service

---
+++ submit.php
@@ -0,0 +1,17 @@
+<?php
+  $url = 'http://eventargs.com/tixers';
+  $destination = 'fiveisprime@me.com';
+
+  $name = $_POST['Name'];
+  $email = $_POST['Email'];
+  $city = $_POST['City'];
+  $state = $_POST['State'];
+
+  $message = $name . ' wants to know when Tixers is live. Contact them at ' . $email . '. They are in ' . $city . ', ' . $state;
+  $message = wordwrap($message, 70, "\r\n");
+
+  mail($destination, 'Somebody wants to know about Tixers.', $message);
+
+  header('Location: ' . $url);
+  exit;
+?>
